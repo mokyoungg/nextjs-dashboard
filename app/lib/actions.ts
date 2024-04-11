d0aaad0f10de1cb8440d3a479eecf1efd8e8aa7c
mokyoungg <mokyoungg@gmail.com>
2024-04-11 15:47:30 +0900

    feat: validate edit invoices from server side actions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -69,13 +69,25 @@ export const createInvoice = async (prevState: State, formData: FormData) => {
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export const updateInvoice = async (id: string, formData: FormData) => {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export const updateInvoice = async (
+  id: string,
+  prevState: State,
+  formData: FormData,
+) => {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
 
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
 
   try {
